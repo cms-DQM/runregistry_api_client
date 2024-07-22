8c55814600aa55737d2e715a20f47edb118cb7c2
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-07-22 21:04:44 +0200

    Fix SSO audience

--- runregistry/__init__.py
+++ runregistry/__init__.py
@@ -2,4 +2,4 @@ from .runregistry import *
 
 # To update:
 # pip install wheel && pip install twine && python setup.py bdist_wheel && twine upload --skip-existing dist/*
-__version__ = "1.2.0"
+__version__ = "1.2.1"
