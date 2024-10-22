1e7a6dd8f6ec78ece623e84fe277725b1ec63626
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-10-22 13:31:55 +0200

    bump version to v1.4.1

--- runregistry/__init__.py
+++ runregistry/__init__.py
@@ -2,4 +2,4 @@ from .runregistry import *
 
 # To update:
 # pip install wheel && pip install twine && python setup.py bdist_wheel && twine upload --skip-existing dist/*
-__version__ = "1.3.1"
+__version__ = "1.4.1"
