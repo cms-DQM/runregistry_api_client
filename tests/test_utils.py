a6c4a7d18d27b1f8d66ea0fd792790b85212ba9a
Dimitris Papagiannis <d.papag.a@gmail.com>
2025-02-13 12:45:01 +0100

    Add User-Agent to requests
    - ...this will make it easy to triage requests
    at the nginx level whenever we need
    to transition between proxies, e.g.,
    the auth proxy, without having users to
    do anything apart upgrading their client.
    - Some tests are added
    - Functions to access some global vars also added.
    - Move __version__ into runregistry.py to be
      usable with the headers.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,15 @@
+import sys
+import pytest
+import requests
 from runregistry.utils import transform_to_rr_run_filter
+from runregistry.runregistry import (
+    __version__,
+    _get_user_agent,
+    _get_headers,
+    _get_token,
+    _get_target,
+    setup,
+)
 
 
 class TestFilterCreation:
@@ -30,3 +41,35 @@ class TestFilterCreation:
         }
 
         assert transform_to_rr_run_filter(run_filter=user_input) == desired_output
+
+
+class TestUtils:
+    def test_user_agent(self):
+        ua = _get_user_agent()
+        assert (
+            __version__ in ua
+            and "runregistry_api_client" in ua
+            and f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+            in ua
+            and requests.__version__ in ua
+            and "zodiac sign" not in ua
+        )
+
+    def test_runregistry_setup(self):
+        for target in ["development", "local", "production"]:
+            setup(target)
+
+            assert _get_target() == target
+
+        with pytest.raises(Exception):
+            setup("HAHAHAHA >:)")
+
+    def test_headers(self):
+        headers = _get_headers(token="WHATEVER :/")
+        assert all(
+            [key in headers for key in ["User-Agent", "Authorization", "Content-type"]]
+        )
+
+    def test_get_token(self):
+        setup("local")
+        assert _get_token() == ""
